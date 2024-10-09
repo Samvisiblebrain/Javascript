868c7da07ffac64989070bc3d36cf736667d6eab
Sam <krishnanisha9@gmail.com>
2024-10-09 13:42:57 +0530

    Initial commit

---
+++ demo.js
@@ -0,0 +1,31 @@
+    document.write("Hi");
+    console.log("hello");
+    document.write("<br>");
+    var ab= ("hi")
+    document.write(ab);
+    document.write("<br>");
+    var a = "hello"
+    // String
+    document.write("hello");
+    document.write("<br>");
+    var b =  "hi"
+    document.write("Hi");
+    // integer
+    document.write("<br>");
+    var c = 23;
+    document.write(23);
+    // flout
+    document.write("<br>");
+    var d = 4.5;
+    document.write(4.5);
+    // Boolean
+    document.write("<br>");
+    var e = true;
+    document.write(true);
+    // value
+    document.write("<br>");
+    var f = false;
+    document.write(false)
+
+
+    
